3af171782d2c5c88a04092776011da11650adc85
dcronkite <dcronkite@gmail.com>
2020-06-18 16:46:02 -0700

    Added mock login component

--- api/socketchat/__main__.py
+++ api/socketchat/__main__.py
@@ -1,9 +1,13 @@
+import random
+
 import flask
+from flask_cors import CORS, cross_origin
 
 from flask_socketio import SocketIO, emit
 
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'a;lskdfja;lsdkfj'
+cors = CORS(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 
@@ -13,9 +17,11 @@ def index():
     emit('index_response', {'data': 'Server'})
 
 
-@app.route('/update')
-def update():
-    return
+@app.route('/login', methods=['POST'])
+@cross_origin()
+def login():
+    """Login user, jwt, etc."""
+    return flask.jsonify({'username': f'User {random.randint(1000, 9999)}'})
 
 
 @socketio.on('connect')
@@ -25,7 +31,10 @@ def handle_connection():
 
 @socketio.on('chat message')
 def handle_chat_message(message):
-    print(f'received json: {message}')
+    data = {
+        'message': message,
+    }
+    emit('chat message', data, broadcast=True)
 
 
 if __name__ == '__main__':
